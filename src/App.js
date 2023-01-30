45d92ce2f193831207400b24e1303ee88c875020
Brad White <bradscottwhite@gmail.com>
2023-01-30 18:23:17 -0500

    added new img concept

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import bg from './beans-grad.jpg';
+//import bg from './rays-grad.png';
 
 const App = () => {
   return (
-    <div>
+    <div> {/*className='overflow-hidden bg-[#111827]'>*/}
       {/*<div className='fixed w-screen h-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100' />*/}
       <div className='fixed w-screen h-full bg-gradient-to-br from-lime-200 via-slate-100 to-teal-300' />
       {/*<img src={bg} className='absolute -top-[1rem] left-1/2 -ml-[40rem] w-[163.125rem] max-w-none sm:-ml-[67.5rem]' alt='background'/>*/}
+      {/*<img src={bg} className='absolute' alt='background'/>*/}
       <div className='relative flex justify-center items-center w-screen h-screen'>
         <h1 className='font-extrabold text-5xl text-slate-800 drop-shadow-lg shadow-slate-500/50'>Check out this cool blur background gradient!!!</h1>
       </div>
